28a7699b5d548391aa76ee7a37b4033127172732
Kir <tamamusiiro@qq.com>
2019-09-14 06:48:47 +0000

    web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('help', 'StaticPageController@help')->name('help');
 Route::get('about', 'StaticPageController@about')->name('about');
 
 Route::resource('user', 'UserController');
+
+Route::get('login', 'SessionController@create')->name('login');
+Route::post('login', 'SessionController@store')->name('login');
+Route::delete('logout', 'SessionController@destroy')->name('logout');
